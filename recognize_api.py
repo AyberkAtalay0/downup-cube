a2287e600d9890d60faded4803061ef33f893b4e
Ayberk Atalay <105879357+AyberkAtalay0@users.noreply.github.com>
2023-12-27 15:43:22 +0300

    Create recognize_api.py

---
+++ recognize_api.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def recognize_shapes_by_lines():
+    data = json.loads(request.data)
+    return str(data)[:10]
+    
+app.run()
